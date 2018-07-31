87d04afe8a3aa07a5c53cdea3d1bfd65af745d8c
Hosen Poursaeid <poursaeid@outlook.com>
2018-07-30 22:55:29 -0400

    flux pattern done

--- src/client/app/components/app.jsx
+++ src/client/app/components/app.jsx
@@ -3,28 +3,48 @@ import React from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import ProductList from './productList.jsx';
-import products from '../lib/products.json';
+import API from '../lib/API';
+import LinkStore from '../stores/LinkStore';
 
 import { Grid } from 'react-bootstrap';
+
+let _getAppState = () => {
+  return { links: LinkStore.getAll()}
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+   /* this.state = {
       rowData: products
-    };
+    }; */
+
+    this.state = _getAppState();
+        this.onChange = this.onChange.bind(this);
   }
 
-  render() {
-    console.log(this.state);
+  componentDidMount(){
+    API.fetchLink();
+    LinkStore.on("change" , this.onChange)
+}
 
+componentWillMount(){
+    LinkStore.removeAllListeners("change", this.onChange);
+}
+
+onChange() {
+    console.log("4- in the view");
+    this.setState(_getAppState());
+}
+
+  render() {
     return (
       <div>
         <Header />
         <Grid id="content">
         <ProductList
-          products={this.state.rowData} />
+          products={this.state.links} />
         </Grid>
         <Footer />
       </div>
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app.jsx';
-import Main from './components/Main'
 
-render( <Main />, document.getElementById('app'));
+render( <App />, document.getElementById('app'));
