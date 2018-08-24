4cbbfe8a5ff0db99f0d2128eff6c31a4008d224c
Hosen Poursaeid <poursaeid@outlook.com>
2018-08-24 07:40:12 -0400

    cleaning extra files

--- src/client/app/components/app.jsx
+++ src/client/app/components/app.jsx
@@ -1,6 +1,5 @@
 import React , {Component} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Grid } from 'react-bootstrap';
 
 import API from '../lib/API';
 import LinkStore from '../stores/LinkStore';
